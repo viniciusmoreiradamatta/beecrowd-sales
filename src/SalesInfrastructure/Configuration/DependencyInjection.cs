8f7b7c0a436cbba400e508cf64d3001627f7aeba
VINICIUS\vinic <tetris.vmata@terceiros.mag.com.br>
2025-04-28 10:33:31 -0300

    feature(docker-compose) add docker suport and docker-compose

--- src/SalesInfrastructure/Configuration/DependencyInjection.cs
+++ src/SalesInfrastructure/Configuration/DependencyInjection.cs
@@ -6,42 +6,41 @@ using SalesDomain.Interfaces.UnitOfWork;
 using SalesInfrastructure.Data;
 using SalesInfrastructure.Data.Repository;
 
-namespace SalesInfrastructure.Configuration
+namespace SalesInfrastructure.Configuration;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddDbContext(configuration)
-                    .AddRepositories();
+        services.AddDbContext(configuration)
+                .AddRepositories();
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddDbContext<SalesDbContext>(options =>
-                options.UseNpgsql(configuration.GetConnectionString("SalesApiDb"), c => c.MigrationsAssembly("SalesInfrastructure")));
+    private static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<SalesDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("SalesApiDb"), c => c.MigrationsAssembly("SalesInfrastructure")));
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddRepositories(this IServiceCollection service)
-        {
-            service.AddScoped<IUnitOfWork, UnitOfWork>();
-            service.AddScoped<ISaleRepository, SaleRepository>();
-            service.AddScoped<IProductRepository, ProductRepository>();
+    private static IServiceCollection AddRepositories(this IServiceCollection service)
+    {
+        service.AddScoped<IUnitOfWork, UnitOfWork>();
+        service.AddScoped<ISaleRepository, SaleRepository>();
+        service.AddScoped<IProductRepository, ProductRepository>();
 
-            return service;
-        }
+        return service;
+    }
 
-        public static async Task ApplyMigration(this IServiceProvider service)
-        {
-            using var scope = service.CreateScope();
+    public static async Task ApplyMigration(this IServiceProvider service)
+    {
+        using var scope = service.CreateScope();
 
-            var db = scope.ServiceProvider.GetRequiredService<SalesDbContext>();
+        var db = scope.ServiceProvider.GetRequiredService<SalesDbContext>();
 
-            await db.Database.MigrateAsync();
-        }
+        await db.Database.MigrateAsync();
     }
 }
